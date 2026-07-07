648316c3251dd3782758b7ddf668bfe8ac2e1939
Prince <princebth1988@gmail.com>
2026-07-07 15:39:03 +0530

    Update server to listen on all interfaces

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.get("/", (req, res) => res.send("TrackMap V2 API Running 🚀"));
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}
-
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
