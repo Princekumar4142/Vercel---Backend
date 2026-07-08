00435dc2ae39c4743fd87ccb34370b9a7685b6b6
Prince <princebth1988@gmail.com>
2026-07-08 07:17:50 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -12,14 +12,11 @@ const otpStore = {};
 // Transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
